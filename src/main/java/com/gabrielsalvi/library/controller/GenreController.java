c6efc12b3fcadc2600a767a4a95ef14111b78645
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2021-10-30 16:15:07 -0300

    feat: add the operation to UPDATE a genre by id

--- src/main/java/com/gabrielsalvi/library/controller/GenreController.java
+++ src/main/java/com/gabrielsalvi/library/controller/GenreController.java
@@ -1,6 +1,7 @@
 package com.gabrielsalvi.library.controller;
 
 import com.gabrielsalvi.library.entity.Genre;
+import com.gabrielsalvi.library.exception.GenreNotFoundException;
 import com.gabrielsalvi.library.repository.GenreRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +10,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/genre")
+@RequestMapping("/api/genres")
 public class GenreController {
 
     private GenreRepository genreRepository;
@@ -27,5 +28,15 @@ public class GenreController {
     @GetMapping
     public List<Genre> listGenres() {
         return genreRepository.findAll();
+    }
+
+    @PutMapping("/{id}")
+    public void updateById(@PathVariable Long id, @RequestBody @Valid Genre genre) throws GenreNotFoundException{
+        Genre genreToUpdate = genreRepository.findById(id).orElseThrow(() -> new GenreNotFoundException(id));
+
+        genreToUpdate.setGenre(genre.getGenre());
+        genreToUpdate.setDescription(genre.getDescription());
+
+        genreRepository.save(genreToUpdate);
     }
 }
